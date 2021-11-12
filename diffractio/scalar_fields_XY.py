3eee859e660c9cf8b90689a4e370a85bd2f69197
luis miguel <optbrea@ucm.es>
2021-11-12 17:02:30 +0100

    normalization fft

--- diffractio/scalar_fields_XY.py
+++ diffractio/scalar_fields_XY.py
@@ -523,7 +523,12 @@ class Scalar_field_XY(object):
             (np.array or Scalar_field_X or None): FFT of the input field
         """
 
+        k = 2 * np.pi / self.wavelength
+
         ttf1 = fft2(self.u)
+
+        ttf1 = ttf1 * np.exp(1j * k * (z + (self.X**2 +
+                                            self.Y**2) / (2 * z))) / (1j * self.wavelength * z)
 
         if remove0 is True:
             ttf1[0, 0] = 0
@@ -560,7 +565,7 @@ class Scalar_field_XY(object):
             self.y = ky
             self.X, self.Y = ndgrid(self.x, self.y)
 
-    def ifft(self, z=10 * mm, shift=True, remove0=True, matrix=False):
+    def ifft(self, z=10 * mm, shift=True, remove0=True, matrix=False, new_field=False):
         """Fast Fourier Transform (fft) of the field.
 
         Parameters:
@@ -573,8 +578,11 @@ class Scalar_field_XY(object):
         Returns:
             (np.array or Scalar_field_X or None): FFT of the input field
         """
+        k = 2 * np.pi / self.wavelength
 
         ttf1 = ifft2(self.u)
+        ttf1 = ttf1 * np.exp(-1j * k * (z + (self.X**2 +
+                                             self.Y**2) / (2 * z))) / (-1j * self.wavelength * z)
 
         if remove0 is True:
             ttf1[0, 0] = 0
@@ -584,34 +592,50 @@ class Scalar_field_XY(object):
 
         if matrix is True:
             return ttf1
+
+        u3 = Scalar_field_XY(self.x, self.y, self.wavelength)
+
+        # x scaling - Infor
+        num_x = self.x.size
+        delta_x = self.x[1] - self.x[0]
+        freq_nyquist_x = 1 / (2 * delta_x)
+        kx = linspace(-freq_nyquist_x, freq_nyquist_x,
+                      num_x) * self.wavelength
+        num_y = self.y.size
+        delta_y = self.y[1] - self.y[0]
+        freq_nyquist_y = 1 / (2 * delta_y)
+        ky = linspace(-freq_nyquist_y, freq_nyquist_y,
+                      num_y) * self.wavelength
+
+        u3.x = kx
+        u3.y = ky
+
+        u3.X, u3.Y = ndgrid(u3.x, u3.y)
+
+        if z is None:
+            u3.x = kx  # exit in angles (radians)
         else:
-            u3 = Scalar_field_XY(self.x, self.y, self.wavelength)
+            u3.x = kx * z  # exit distances at a obsrvation plane z
 
-            # x scaling - Infor
-            num_x = self.x.size
-            delta_x = self.x[1] - self.x[0]
-            freq_nyquist_x = 1 / (2 * delta_x)
-            kx = linspace(-freq_nyquist_x, freq_nyquist_x,
-                          num_x) * self.wavelength
-            num_y = self.y.size
-            delta_y = self.y[1] - self.y[0]
-            freq_nyquist_y = 1 / (2 * delta_y)
-            ky = linspace(-freq_nyquist_y, freq_nyquist_y,
-                          num_y) * self.wavelength
+        u3.u = ttf1
 
-            u3.x = kx
-            u3.y = ky
+        if new_field is True:
+            field_output = Scalar_field_XY(self.x, self.y, self.wavelength)
+            field_output.x = kx
+            field_output.y = ky
 
-            u3.X, u3.Y = ndgrid(u3.x, u3.y)
+            field_output.X, field_output.Y = ndgrid(field_output.x,
+                                                    field_output.y)
+            field_output.u = ttf1
 
-            if z is None:
-                u3.x = kx  # exit in angles (radians)
-            else:
-                u3.x = kx * z  # exit distances at a obsrvation plane z
+            return field_output
+        else:
+            self.u = ttf1
+            self.x = kx
+            self.y = ky
+            self.X, self.Y = ndgrid(self.x, self.y)
 
-            u3.u = ttf1
-
-            return u3
+        return u3
 
     def _RS_(self,
              z,
